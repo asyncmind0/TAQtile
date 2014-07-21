5309277fc8842cf97f5ccc680c2941f02b1651fd
Steven Joseph <steven@stevenjoseph.in>
2014-07-21 10:33:30 +1000

    addedpriority  notifier

---
+++ priority_notify.py
@@ -0,0 +1,22 @@
+from libqtile import widget
+
+
+class PriorityNotify(widget.base.ThreadedPollText):
+    defaults = [
+        ("graph_color", "18BAEB", "Graph color"),
+        ("fill_color", "1667EB.3", "Fill color for linefill graph"),
+        ("border_color", "215578", "Widget border color"),
+        ("border_width", 2, "Widget border width"),
+        ("margin_x", 3, "Margin X"),
+        ("margin_y", 3, "Margin Y"),
+        ("samples", 100, "Count of graph samples."),
+        ("frequency", 1, "Update frequency in seconds"),
+        ("type", "linefill", "'box', 'line', 'linefill'"),
+        ("line_width", 3, "Line width"),
+        ("start_pos", "bottom", "Drawer starting position ('bottom'/'top')"),
+        ("background", '18BAEB', "Widget background color")
+    ]
+
+    def __init__(self, width=100, **config):
+        widget.base.ThreadedPollText.__init__(self, **config)
+        self.add_defaults(PriorityNotify.defaults)
