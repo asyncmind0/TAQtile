2d33e2402f95e283fdcb6259e2569d0d90772567
Steven Joseph <steven@stevenjoseph.in>
2015-12-22 08:48:59 +1100

    passmenu Added notification on failure
    - sanity check output

--- passmenu.py
+++ passmenu.py
@@ -3,15 +3,17 @@ from __future__ import print_function
 import logging
 import os
 
+MAX_PASS = 200
 log = logging.getLogger("qtile")
 
 
 def passmenu(qtile, args):
-    from plumbum import local
+    from plumbum import local, ProcessExecutionError
     try:
         dmenu = local['dmenu']
         xdotool = local['xdotool']
         envoyexec = local['envoy-exec']
+        notify_send = local['notify-send']
 
         with local.cwd(os.path.expanduser("~/.password-store/")):
             passfiles = [
@@ -24,9 +26,22 @@ def passmenu(qtile, args):
         args.extend(['-p', 'Pass:'])
 
         selection = (dmenu[args] << '\n'.join(reversed(passfiles)))(
-            retcode=None)
+            retcode=None).strip()
+        if not selection:
+            return
+        try:
+            password = envoyexec('pass', 'show', selection)
+        except ProcessExecutionError as e:
+            logging.exception("Error getting pass output")
+            notify_send(str(e))
+        password = password.decode('utf8').strip()
+        if not password:
+            return
+        if len(password) > MAX_PASS:
+            notify_send("Pass out put larger than %s, skipping." % MAX_PASS)
+            return
         xdotool(
             'type', '--clearmodifiers',
-            envoyexec('pass', 'show', selection.strip()).split('\n')[0].strip())
+            password)
     except:
         logging.exception("Error getting pass")
