67addf7b84fbe1200e632f49bbd72d8666fdf025
Steven Joseph <steven@stevenjoseph.in>
2016-05-10 22:43:05 +1000

    Updated multiscreengroupbox ddraw method

--- widgets/multiscreengroupbox.py
+++ widgets/multiscreengroupbox.py
@@ -1,3 +1,4 @@
+from libqtile.log_utils import logger as log
 from libqtile import widget
 
 
@@ -19,17 +20,17 @@ class MultiScreenGroupBox(widget.GroupBox):
     def get_label(self, group):
         return self.namemap.get(group)
 
-
     def draw(self):
         self.drawer.clear(self.background or self.bar.background)
-
         offset = 0
-        for i, g in enumerate(self.qtile.groups):
-            is_block = (self.highlight_method == 'block')
+        for i, g in enumerate(self.groups):
             gtext = self.get_label(g.name)
             #log.debug(gtext)
             if not gtext:
                 continue
+            to_highlight = False
+            is_block = (self.highlight_method == 'block')
+            is_line = (self.highlight_method == 'line')
 
             bw = self.box_width([g])
 
@@ -48,26 +49,33 @@ class MultiScreenGroupBox(widget.GroupBox):
                     if self.bar.screen.group.name == g.name:
                         if self.qtile.currentScreen == self.bar.screen:
                             border = self.this_current_screen_border
+                            to_highlight = True
                         else:
                             border = self.this_screen_border
                     else:
                         border = self.other_screen_border
             elif self.group_has_urgent(g) and \
-                    self.urgent_alert_method in ('border', 'block'):
+                    self.urgent_alert_method in ('border', 'block', 'line'):
                 border = self.urgent_border
                 if self.urgent_alert_method == 'block':
                     is_block = True
+                elif self.urgent_alert_method == 'line':
+                    is_line = True
             else:
                 border = self.background or self.bar.background
 
             self.drawbox(
                 self.margin_x + offset,
-                self.get_label(g.name),
+                gtext,
+                #g.name,
                 border,
                 text_color,
-                self.rounded,
-                is_block,
-                bw - self.margin_x * 2 - self.padding_x * 2
+                highlight_color=self.highlight_color,
+                width=bw - self.margin_x * 2 - self.padding_x * 2 - 1,
+                rounded=self.rounded,
+                block=is_block,
+                line=is_line,
+                highlighted=to_highlight
             )
             offset += bw
         self.drawer.draw(offsetx=self.offset, width=self.width)
