4f6af08d2359558342dc42d93691d88b8e7bc386
Steven Joseph <steven@stevenjoseph.in>
2016-08-19 22:29:46 +1000

    Fix password unlock type and focus

--- passmenu.py
+++ passmenu.py
@@ -1,56 +1,37 @@
 from __future__ import print_function
 
-import logging
 import os
+from os.path import dirname, join
 from dmenu import dmenu_show
 from recent_runner import RecentRunner
+from libqtile.log_utils import logger as log
+from plumbum import local
+from time import sleep
 
 MAX_PASS = 200
-log = logging.getLogger("qtile")
 
 
 def passmenu(qtile, args):
-    recent = RecentRunner('pass_menu')
-    from plumbum import local, ProcessExecutionError
     try:
-        xdotool = local['xdotool']
-        envoyexec = local['envoy-exec']
-        notify_send = local['notify-send']
-
+        recent = RecentRunner('pass_menu')
         with local.cwd(os.path.expanduser("~/.password-store/")):
             passfiles = [
                 os.path.splitext(os.path.join(base, f))[0][2:]
                 for base, _, files in os.walk('.')
                 for f in files if f.endswith(".gpg")
             ]
-
         args = args.split(' ')
         args.extend(['-p', 'Pass:'])
-        log.error(passfiles)
-
         selection = dmenu_show("pass", recent.list(passfiles))
         if not selection:
             return
-        password = None
-        with local.env(DISPLAY=''):
-            try:
-                password = envoyexec(
-                    'pass', 'show',
-                    selection,
-                    timeout=5,
-                )
-            except ProcessExecutionError as e:
-                logging.exception("Error getting pass output")
-                notify_send("-u", "critical", "GPG is locked ?")
-        password = password.strip()
-        if not password:
-            return
-        if len(password) > MAX_PASS:
-            notify_send("-u", "critical", "Pass out put larger than %s, skipping." % MAX_PASS)
-            return
-        xdotool(
-            'type', '--clearmodifiers',
-            password)
+        qtile.cmd_spawn(
+            [
+                join(dirname(__file__), "bin", "passinsert"),
+                selection,
+                str(qtile.currentWindow.window.wid),
+            ]
+        )
         recent.insert(selection)
     except:
-        logging.exception("Error getting pass")
+        log.exception("Error getting pass")
