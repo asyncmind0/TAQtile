7d45f416584ab78020160388ee5fa7a1785fb5e9
Steven Joseph <steven@stevenjoseph.in>
2022-04-10 14:15:02 +1000

    Fix pinentry float config

--- config.py
+++ config.py
@@ -147,3 +147,17 @@ screens = get_screens(num_monitors, groups)
 class NoTimerFilter(logging.Filter):
     def filter(self, record):
         return "timer" not in record.getMessage()
+
+
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(wm_class="pinentry-qt"),  # GPG key password entry
+    ]
+)
