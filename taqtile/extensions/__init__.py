71bc788b1ec062238c5bf241ec58f3d7b298101b
Steven Joseph <steven@stevenjoseph.in>
2022-04-25 00:52:16 +1000

    Improve kill window list implementation

--- taqtile/extensions/__init__.py
+++ taqtile/extensions/__init__.py
@@ -11,6 +11,9 @@ from libqtile import hook
 from libqtile.extension.dmenu import Dmenu, DmenuRun
 
 from libqtile.extension.window_list import WindowList as QWindowList
+from libqtile.scratchpad import ScratchPad
+
+from libqtile.backend import base
 
 # from libqtile.extension.window_list import WindowList
 from plumbum import local
@@ -80,7 +83,8 @@ class WindowList(QWindowList):
             screen = self.qtile.current_screen
             screen.set_group(win.group)
 
-        win.group.focus(win)
+        win.group.focus(win, force=True)
+        # win.cmd_focus()
 
 
 @lru_cache()
@@ -96,12 +100,55 @@ def list_executables(ttl_hash=None):
     return set(executables)
 
 
-class KillWindows(WindowList):
+class KillWindows(Dmenu):
+    defaults = [
+        (
+            "item_format",
+            "{group}.{id}: {window}",
+            "the format for the menu items",
+        ),
+        (
+            "all_groups",
+            True,
+            "If True, list windows from all groups; otherwise only from the current group",
+        ),
+        ("dmenu_lines", "80", "Give lines vertically. Set to None get inline"),
+    ]
+
+    def __init__(self, **config):
+        Dmenu.__init__(self, **config)
+        self.add_defaults(KillWindows.defaults)
+
+    def list_windows(self):
+        id = 0
+        self.item_to_win = {}
+
+        if self.all_groups:
+            windows = [
+                w
+                for w in self.qtile.windows_map.values()
+                if isinstance(w, base.Window)
+            ]
+        else:
+            windows = self.qtile.current_group.windows
+
+        for win in windows:
+            if win.group and not isinstance(win.group, ScratchPad):
+                item = self.item_format.format(
+                    group=win.group.label or win.group.name,
+                    id=id,
+                    window=win.name,
+                )
+                self.item_to_win[item] = win
+                id += 1
+
     def run(self):
         self.list_windows()
         self.dmenu_prompt = "Kill selected windows <Ctrk-Ret> to select:"
         self._configure(self.qtile)
-        windows = Dmenu.run(self, items=self.item_to_win.keys()).split("\n")
+        windows = super().run(items=self.item_to_win.keys()).split("\n")
+
+        logger.debug("selected killing window: %s", windows)
         for win in windows:
             win = win.strip()
             if not win:
