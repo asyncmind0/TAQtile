08fd8daf3f7de0e56aade3c04f00d717f4ed3cb3
Steven Joseph <steven@stevenjoseph.in>
2023-05-03 19:00:38 +1000

    Fix add support for active_background and inactive_background in togglebutton

--- taqtile/widgets/togglebtn.py
+++ taqtile/widgets/togglebtn.py
@@ -24,7 +24,9 @@ class ToggleButton(GenPollText):
             [
                 ("active", False),
                 ("active_text", "\uf205"),  # use fontawesome
+                ("active_background", None),  # use fontawesome
                 ("inactive_text", "\uf204"),
+                ("inactive_background", None),  # use fontawesome
                 ("on_command", "notify-send switch %(name)s on"),
                 ("off_command", "notify-send switch %(name)s off"),
                 (
@@ -39,19 +41,25 @@ class ToggleButton(GenPollText):
                 ("func", self.check_state, "Poll Function"),
             ]
         )
+        self.default_background = self.background
         self.check_state()
+
+    def _update_background(self):
+        self.background = (
+            self.active_background
+            if self.active
+            else (self.inactive_background or self.default_background)
+        )
 
     def check_state(self):
         global TOGGLE_BUTTON_STATES
         # logger.debug(f"calling {self.check_state_command}")
         # python check status of command string executed in a shell
         if self.check_state_command:
-
             # Execute the command
             with subprocess.Popen(
                 self.check_state_command, shell=True
             ) as process:
-
                 # Wait for the command to complete and get the return code
                 return_code = process.wait()
             if return_code == 0:
@@ -60,6 +68,7 @@ class ToggleButton(GenPollText):
                 # logger.debug(f"status {status} {self.check_state_command}")
                 self.active = False
         TOGGLE_BUTTON_STATES[self.name] = self.active
+        self._update_background()
         return self.active_text if self.active else self.inactive_text
 
     def _configure(self, qtile, bar):
