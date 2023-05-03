812d936bbe5035d2c74d54d95c989e7b7589e7c3
Steven Joseph <steven@stevenjoseph.in>
2023-05-03 19:01:27 +1000

    Fix wraparound for max layout

--- taqtile/layouts.py
+++ taqtile/layouts.py
@@ -1,39 +1,21 @@
 import logging
 from libqtile.layout.max import Max as QMax
-from libqtile.command.base import expose_command
-from taqtile.log import logger
 
-logger = logging.getLogger("taqtile")
+logger = logging.getLogger(__name__)
 
 
 class Max(QMax):
-    @expose_command("previous")
-    def up(self):
-        if self.clients.current_client is None:
-            client = self.clients[0]
-            logger.error(f"up: current client {client}")
-        else:
-            idx = self.clients.index(self.clients.current_client) - 1
-            if idx < 0:
-                idx = len(self.clients.clients) - 1
-            client = self.clients.clients[idx]
-            logger.error(
-                f"up: current client {client}  {self.clients.index(client)+1}/{len(self.clients)}"
-            )
-        self.group.focus(client, True)
+    def add_client(self, client, offset_to_current=0, client_position=None):
+        if client in self.clients:
+            return
+        return self.clients.add_client(
+            client, offset_to_current, client_position
+        )
 
-    @expose_command("next")
-    def down(self):
-        if self.clients.current_client is None:
-            client = self.clients[0]
-            logger.error(f"down: current client {client}")
-        else:
-            idx = self.clients.index(self.clients.current_client) + 1
-            if idx == len(self.clients.clients):
-                idx = 0
+    def focus_previous(self, window):
+        client = self.clients.focus_previous(window)
+        return self.clients[-1] if not client else client
 
-            client = self.clients.clients[idx]
-            logger.error(
-                f"down: current client {client}  {self.clients.index(client)+1}/{len(self.clients)}"
-            )
-        self.group.focus(client, True)
+    def focus_next(self, window):
+        client = self.clients.focus_next(window)
+        return self.clients[0] if not client else client
